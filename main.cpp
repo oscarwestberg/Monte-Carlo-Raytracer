8d515a77baef89a196337b6bcb428f3fb5b9c23f
Syllvan <johansylvan1@gmail.com>
2015-10-19 17:03:40 +0200

    Added basic setup

--- main.cpp
+++ main.cpp
@@ -8,6 +8,20 @@
 
 const int screenWidth = 800, screenHeight = 600;
 
+bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
+{
+	Vec3f l = center - rayorig;
+	float tca = l.dot(raydir);
+	if (tca < 0) return false;
+	float d2 = l.dot(l) - tca * tca;
+	if (d2 > radius2) return false;
+	float thc = sqrt(radius2 - d2);
+	t0 = tca - thc;
+	t1 = tca + thc;
+
+	return true;
+}
+
 int main() {
     glm::vec3 radianceArray[screenWidth * screenHeight];
     
