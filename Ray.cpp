386172578b3fb83c91e8251c7602b0e82ddce1df
Syllvan <johansylvan1@gmail.com>
2015-10-21 13:36:51 +0200

    added ray trace where another color is return on intersection

--- Ray.cpp
+++ Ray.cpp
@@ -4,27 +4,35 @@
 //
 
 #include "Ray.hpp"
+#define INF 9999
 
 Ray::Ray(Scene *s) {
     scene = s;
 }
 
 glm::vec3 Ray::trace(glm::vec3 rayOrig, glm::vec3 rayDir, float depth, int bounces) {
-    glm::vec3 c(0.0, 255.0, 100.0);
-    
-    // Compare ray with every object in scene
-    // Find the smallest distance to an object
-    for (int i = 0; i < scene->objects->size(); i++) {
-        
-    }
-    
-    // If we found an intersection, cast shadow ray
-    // Compare shadow ray with every object
-    
-    
-    // If point isn't in shadow, update with object's color and light's brightness
-    
-    
-    
-    return c;
+	glm::vec3 c(0.0, 255.0, 100.0);
+	glm::vec3 c2(255.0, 0, 100.0);
+
+	// Compare ray with every object in scene
+	// Find the smallest distance to an object
+	float t0, t1, nearestIntersection = INF;
+	for (auto &o : *scene->objects) {
+		if (o.intersects(rayOrig, rayDir, t0, t1)) {
+			if (nearestIntersection > t0) nearestIntersection = t0;
+			return c2;
+		}
+	}
+	glm::vec3 intersectPos = rayOrig + nearestIntersection*rayDir;
+	// If we found an intersection, cast shadow ray
+	// Compare shadow ray with every object
+	//Ray ray(&scene);
+	//rayDir = scene->lightPos1 - intersectPos;
+	//glm::vec3 color = ray.trace(intersectPos, rayDir, 0.0, 0);
+
+	// If point isn't in shadow, update with object's color and light's brightness
+
+
+
+	return c;
 }
